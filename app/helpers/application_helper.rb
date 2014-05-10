e7389a4656f8d18c7eed60b5fbbdcc5b0cf05891
Marcin Brzeziński <cintrzyk@gmail.com>
2014-05-10 18:55:51 +0200

    js assets helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def self.images
+    Dir.glob(Rails.root.join('app/assets/images/**/*.*')).map do |path|
+      path.gsub(Rails.root.join('app/assets/images/').to_s, '')
+    end
+  end
 end
